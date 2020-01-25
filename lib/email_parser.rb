893360feb1bc817c23801f42ee5db109343390e9
Ricky dildy <rdildy92@gmail.com>
2020-01-25 02:34:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,7 +1,10 @@
-class EmailParser 
+class EmailAddressParser 
   attr_accessor :email
   
   def initialize (email)
     @email = email 
   end 
 end 
+
+avi@test.com = EmailAddressParser.new 
+arel@test.com = EmailAddressParser.new
